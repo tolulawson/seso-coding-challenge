f0a510f043e68b75fc3be8b10bd1abb765999617
Tolu Lawson <lawsontolu1@gmail.com>
2022-08-25 23:53:15 -0700

    Add implemented solutions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,7 +2,27 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+const { PriorityQueue } = require('@datastructures-js/priority-queue');
+
+module.exports = async (logSources, printer) => {
+  const queue = new PriorityQueue(({ log: a }, { log: b}) => a.date > b.date ? 1 : a.date < b.date ? -1 : 0);
+
+  await Promise.all(logSources.map(async (source, index) => {
+    const log = await source.popAsync();
+    if (log) {
+      queue.push({ log, logSourceIndex: index });
+    }
+}))
+
+  while (!queue.isEmpty()) {
+    const { log, logSourceIndex } = queue.pop();
+    if (log) {
+      printer.print(log);
+      queue.push({ log: await logSources[logSourceIndex].popAsync(), logSourceIndex });
+    }
+  }
+
+  printer.done();
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,26 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const { PriorityQueue } = require('@datastructures-js/priority-queue');
+
 module.exports = (logSources, printer) => {
+
+  // Create a priority queue instance to keep a subset of the logs in memory, ranked by the date property of each log
+  const queue = new PriorityQueue(({ log: a }, { log: b}) => a.date > b.date ? 1 : a.date < b.date ? -1 : 0);
+  
+  // Push the first log and the index of the each the log source into the queue.
+  logSources.forEach((source, index) => {
+    queue.push({ log: source.pop(), logSourceIndex: index });
+  });
+
+  // The highest priority log is popped from the queue and  printed. A new log is popped from the log source of the just printed log and pushed into the queue. This is repeated till the queue is empty.
+  while (!queue.isEmpty()) {
+    const { log, logSourceIndex } = queue.pop();
+    if (log) {
+      printer.print(log);
+      queue.push({ log: logSources[logSourceIndex].pop(), logSourceIndex });
+    }
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
